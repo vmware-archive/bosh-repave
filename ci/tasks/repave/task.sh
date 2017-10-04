5dcc52de593cf0c6a46fd9b615c9fadf53606c2c
Luciano Silva <lsilva@pivotal.io>
2017-10-04 13:48:19 -0700

    Remove comments from output

--- ci/tasks/repave/task.sh
+++ ci/tasks/repave/task.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -eu
 
-# om-linux --request-timeout 7200 export-installation --output-file om-installation/installation.zip
-
 source ./bosh-repave-repo/ci/scripts/export-director-metadata
 
 echo "Logging in to bosh $BOSH_ADDRESS"
@@ -12,12 +10,9 @@ export BOSH_ENVIRONMENT=bosh
 om curl -p /api/v0/deployed/products > deployed_products.json
 
 # loop through deployments
-echo "Iterating through list of deployments - [$DEPLOYMENTS]"
 for deployment in $(echo $DEPLOYMENTS | sed "s/,/ /g")
 do
-  # echo "Processing deployment [${deployment}]"
   DEPLOYMENT_NAME=$(jq --arg deployment "$deployment" -r '.[] | select( .type | contains($deployment)) | .guid' "deployed_products.json")
-  # echo "Found [$DEPLOYMENT_NAME]"
 
   # get list of vms
   JOBS=$(bosh -d $DEPLOYMENT_NAME --json instances -i | jq -r '.Tables[].Rows[] | select(.index=="1") | .instance | split("/")[0]')
@@ -25,16 +20,7 @@ do
   for job in $JOBS
   do
     echo "Recreating job [$job]"
-    bosh -d $DEPLOYMENT_NAME -n recreate --dry-run $job
+    bosh -d $DEPLOYMENT_NAME -n recreate $job
   done
 
-
-
 done
-
-exit 1
-# iterate through white-listed deployments
-
-
-
-  # run bosh recreate all for the deployment
